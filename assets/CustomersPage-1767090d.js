c0952d7f695d82f6d8bfbbe8626d31ae47d3029e
iparraguirrepro <hola@iparraguirre.pro>
2024-10-19 02:55:07 -0500

    .

---
+++ assets/CustomersPage-1767090d.js
@@ -0,0 +1,2 @@
+import{_ as s,r as o,o as t,c,b as r}from"./index-b45d4e71.js";const a={},n={class:"py-4"};function _(i,m){const e=o("RouterView");return t(),c("div",n,[r(e)])}const p=s(a,[["render",_],["__file","C:/Users/ID-04/Documents/Main/vuestic-impala/src/pages/customers/CustomersPage.vue"]]);export{p as default};
+//# sourceMappingURL=CustomersPage-1767090d.js.map
---
+++ assets/OrdersPage-b3f89fa2.js
@@ -0,0 +1,2 @@
+import{_ as s,r as o,o as r,c,b as t}from"./index-b45d4e71.js";const a={},n={class:"py-4"};function _(i,d){const e=o("RouterView");return r(),c("div",n,[t(e)])}const l=s(a,[["render",_],["__file","C:/Users/ID-04/Documents/Main/vuestic-impala/src/pages/orders/OrdersPage.vue"]]);export{l as default};
+//# sourceMappingURL=OrdersPage-b3f89fa2.js.map
